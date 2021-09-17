0a63e0bba040af6352f043c412bae84c378db713
Corey Sheldrick <coreybrendan@me.com>
2021-09-17 15:04:31 -0400

    F/text component atom import is working as expected 🤌 (#8)
    * resolving conflicts
    
    * TextComponent is working as expected
    
    Co-authored-by: Corey Sheldrick <coreysheldrick@Coreys-MacBook-Pro.local>

--- src/core/App.js
+++ src/core/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-import { Button } from '../common/components/atoms'
+import { Button, TextComponent } from '../common/components/atoms'
 
 const App = () => {
   return (
     <div className="App">
       <header>
         <h1>Happy Room Designer (Endy Edition)</h1>
-        <Button label="Click me please!" />
       </header>
+        <TextComponent />
+        <Button label="Click me please!" />
     </div>
   );
 }
