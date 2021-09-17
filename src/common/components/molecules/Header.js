504ce989c08ea9d796cc3173478445bb4ed73e96
annajliang <58713256+annajliang@users.noreply.github.com>
2021-09-17 16:38:41 -0400

    F/header atom (#12)
    * create header component, add some placeholder styles and import to App.js
    
    * whoops, readding this buttom component
    
    * fix issue with hender component not rendering
    
    * add button component to use as button for instructions modal
    
    * upload logo asset
    
    * move header from atom to molecule folder
    
    * add height to logo
    
    * create index.js for molecules and rename paths to direct to molecules folder
    
    * remove classes props
    
    * remove missed class prop
    
    Co-authored-by: Anna <anna@endy.com>

---
+++ src/common/components/molecules/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Anchor} from '../atoms'
+import endyLogo from '../../../assets/endy-logo.svg'
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: pink;
+    height: auto;
+    padding-right: 2rem;
+`
+
+export const Header = () => {
+    return (
+        <StyledHeader>
+            <Anchor          
+                href="https://endy.com" 
+                img={endyLogo} />
+            <h1>Happy Room Designer (Endy Edition)</h1>
+            <Button label="Instructions" />
+        </StyledHeader>
+    )
+}
