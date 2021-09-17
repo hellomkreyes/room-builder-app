5de1690fd4ddaccc56c4ffc566e0044117e61b89
Corey Sheldrick <coreysheldrick@Coreys-MacBook-Pro.local>
2021-09-17 16:34:12 -0400

    moved footer to molecules and changed ipsum

--- src/common/components/atoms/TextComponent.js
+++ src/common/components/atoms/TextComponent.js
@@ -3,13 +3,12 @@ import styled from 'styled-components';
 
 const StyledTextComponent = styled.div`
 	background-color: peachpuff;
-
 `
 
 export const TextComponent = () => {
 	return(
 		<StyledTextComponent>
-			<p>This is an intro copy block!</p>
+			<p>Omae wa mou shindeiru. Anime convention hontou nihongo, Sailor Moon onigiri onii-chan, dame ORA ORA ORA ne weeaboo Fullmetal Alchemist isekai kami-sama shonen onigiri. Waifu Dragon Ball Z kami-sama katakana ORA ORA ORA isekai kimono moe; desu kareshi meganekko, yandere JoJo's Bizarre Adventure kohai shonen. Aishiteru seiyuu senpai notice me yokatta zettai Boku no Hero Academia JoJo's Bizarre Adventure doujinshi weeaboo Death Note sou desu ne harem Bleach bakemono kohai. AMV taiyaki kanashii; josei Fullmetal Alchemist takoyaki anime convention otaku otaku sushi hiragana yokatta Demon Slayer bakemono otaku. Bishoujo itadakimasu pocky Inuyasha OVA chotto Studio Ghibli doujinshi WRYYY anime doki doki Attack on Titan kuudere otouto AMV.</p>
 		</StyledTextComponent>
 	);
 }
--- src/core/App.js
+++ src/core/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Button, TextComponent, Anchor } from '../common/components/atoms'
-import { Footer } from '../common/components/organisms'
+import { Footer } from '../common/components/molecules'
 
 const App = () => {
   return (
