eac87a89c3bb968b7933291893ac5a2e4f3c0e48
Corey Sheldrick <coreysheldrick@Coreys-MacBook-Pro.local>
2021-09-17 14:58:33 -0400

    TextComponent is working as expected

--- src/common/components/atoms/TextComponent.js
+++ src/common/components/atoms/TextComponent.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const TextComponent = () => {
+export const TextComponent = () => {
     return(
         <div>
             <p>This is an intro copy block!</p>
         </div>
     );
 }
-
-export default TextComponent;
