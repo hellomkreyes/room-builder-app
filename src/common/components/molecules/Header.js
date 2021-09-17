3d0f789a6fbe5cb90cb1e0ef6ebfcdbe43dfc9a9
Anna <anna@endy.com>
2021-09-17 16:18:06 -0400

    move header from atom to molecule folder

--- src/common/components/molecules/Header.js
+++ src/common/components/molecules/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Button } from '../atoms'
+import { Button, Anchor} from '../atoms'
+import endyLogo from '../../../assets/endy-logo.svg'
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
@@ -7,13 +8,17 @@ const StyledHeader = styled.header`
     align-items: center;
     justify-content: space-between;
     background-color: pink;
-    height: 50px;
-    padding: 0 2rem;
+    height: auto;
+    padding-right: 2rem;
 `
 
 export const Header = () => {
     return (
         <StyledHeader>
+            <Anchor          
+                href="https://endy.com" 
+                img={endyLogo}
+                classes="width-100" />
             <h1>Happy Room Designer (Endy Edition)</h1>
             <Button label="Instructions" />
         </StyledHeader>
