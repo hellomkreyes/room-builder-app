8bb1fd51ef9fce9193f8c9cf0f02caad09f05e93
Corey Sheldrick <coreybrendan@me.com>
2021-09-17 17:17:42 -0400

    C/footer update pre-weekend smol changes 🐭 (#13)
    * moved footer to molecules and changed ipsum
    
    * Minor changes and cheeky updates (ipsum)
    
    Co-authored-by: Corey Sheldrick <coreysheldrick@Coreys-MacBook-Pro.local>

--- src/core/App.js
+++ src/core/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import { Button, TextComponent, Anchor } from '../common/components/atoms'
 import { Header } from '../common/components/molecules'
-import { Footer } from '../common/components/organisms'
+import { Footer } from '../common/components/molecules'
 
 const App = () => {
   return (
