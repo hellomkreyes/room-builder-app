f2ec12317fa09068657cc1fed7ace0d6c8bbd459
Anna <anna@endy.com>
2021-09-17 15:24:59 -0400

    add button component to use as button for instructions modal

--- src/common/components/atoms/Header.js
+++ src/common/components/atoms/Header.js
@@ -1,17 +1,21 @@
 import React from 'react';
+import { Button } from '../atoms'
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
     display: flex;
     align-items: center;
+    justify-content: space-between;
     background-color: pink;
     height: 50px;
+    padding: 0 2rem;
 `
 
 export const Header = () => {
     return (
         <StyledHeader>
             <h1>Happy Room Designer (Endy Edition)</h1>
+            <Button label="Instructions" />
         </StyledHeader>
     )
 }
