c6a59a570f383a04bcde40baad60b039994c3fff
Anna <anna@endy.com>
2021-09-17 16:18:53 -0400

    add height to logo

--- src/common/components/atoms/Anchor.js
+++ src/common/components/atoms/Anchor.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
+
+const StyledAnchor = styled.a`
+  img {
+    height: 50px;
+  }
+`
 
 export const Anchor = ({text, href, classes, img}) => {
   return (
-    <a href={href} class={classes} target="_blank" rel="noreferrer">
+    <StyledAnchor href={href} className={classes} target="_blank" rel="noreferrer">
       { img ? <img src={img} alt={text}/> : text}
-    </a>
+    </StyledAnchor>
   )
 }
 
